6e08fad765623c4d2b0f145ee1621a571096c630
Damià Rita <damiarita@gmail.com>
2024-02-25 00:00:38 +0100

    Avoid string concatenation

--- src/poster.tsx
+++ src/poster.tsx
@@ -11,7 +11,7 @@ export default({pictureProps, imgProps, sizes}:Props)=>
 <picture {...pictureProps}>
 {sizes.flatMap(
   ({height, width, mediaQuery, posterSources}, sizeIndex)=>(posterSources.map(
-    ({url, format}, sourceIndex)=>(<source key={sizeIndex.toString()+"-"+sourceIndex.toString()} height={height} width={width} media={mediaQuery} srcSet={url} type={format} />))
+    ({url, format}, sourceIndex)=>(<source key={`${sizeIndex}-${sourceIndex}`} height={height} width={width} media={mediaQuery} srcSet={url} type={format} />))
   )
 )}
 <img {...imgProps} />
