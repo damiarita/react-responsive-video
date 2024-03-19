39545257b8bf93a1d0f37afef883d60fd3217bdb
Damià Rita <damiarita@gmail.com>
2024-03-19 22:17:06 +0100

    Remove unnecessary import

--- __tests__/ssr_tests.jsx
+++ __tests__/ssr_tests.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {create} from 'react-test-renderer';
 import ResponsiveVideo from '../src'
 import {pictureProps, imgProps, videoProps, sizes} from './data/fullProps'
